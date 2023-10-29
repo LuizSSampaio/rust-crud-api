4cf29b7c26f92165fcaa4876a9c4449926eee8a4
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-29 11:15:14 -0300

    Added handle_client, set_database, get_id and get_users_request_body functions

--- src/main.rs
+++ src/main.rs
@@ -29,17 +29,6 @@ fn main() {
         println!("Error: {}", e);
         return;
     }
-}
-
-fn set_database() -> Result<(), PostgresError> {
-    //Connect to database
-    let mut client = Client::connect(DB_URL, NoTls)?;
-
-    //Create table
-    client.execute(
-        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR NOT NULL)",
-        &[],
-    )?;
 
     //Start the server and print port
     let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
@@ -56,4 +45,58 @@ fn set_database() -> Result<(), PostgresError> {
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            let (status_line, content) = match &*request {
+                r if request_with("POST /users") => handle_post_request(r),
+                r if request_with("GET /users/") => handle_get_request(r),
+                r if request_with("GET /users") => handle_get_all_request(r),
+                r if request_with("PUT /users/") => handle_put_request(r),
+                r if request_with("DELETE /users/") => handle_delete_request(r),
+                _ => (NOT_FOUND, "Not Found".to_string()),
+            };
+
+            stream
+                .write_all(format!("{}{}", status_line, content).as_bytes())
+                .unwrap();
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+}
+
+fn set_database() -> Result<(), PostgresError> {
+    //Connect to database
+    let mut client = Client::connect(DB_URL, NoTls)?;
+
+    //Create table
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR NOT NULL)",
+        &[],
+    )?;
+
+    Ok(())
+}
+
+fn get_id(request: &str) -> &str {
+    request
+        .split("/")
+        .nth(2)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default()
+}
+
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
 }
