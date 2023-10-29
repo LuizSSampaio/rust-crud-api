4f189d6733b6a0b7049840614a1ba71bbbdb8591
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-29 14:14:08 -0300

    Added docker support

--- src/main.rs
+++ src/main.rs
@@ -188,9 +188,8 @@ fn set_database() -> Result<(), PostgresError> {
     let mut client = Client::connect(DB_URL, NoTls)?;
 
     //Create table
-    client.execute(
-        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR NOT NULL)",
-        &[],
+    client.batch_execute(
+        "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR NOT NULL)"
     )?;
 
     Ok(())
